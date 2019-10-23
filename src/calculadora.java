ee3886ca2252ea5d6a8c8cfc3b452aa2ab6ca991
thieres.alcalde <thieresalcalde@gmail.com>
2019-10-22 21:53:34 -0300

    Initial commit

---
+++ src/calculadora.java
@@ -0,0 +1,15 @@
+public class calculadora {
+
+    public Double somar(Double valor1, Double valor2){
+        return valor1 + valor2;
+    }
+    public Double subtrair(Double valor1, Double valor2){
+       return valor1 - valor2;
+    }
+    public Double dividir(Double valor1, Double valor2){
+        return valor1 / valor2;
+    }
+    public Double multiplicar(Double valor1, Double valor2){
+        return valor1 * valor2;
+    }
+}
---
+++ src/main.java
@@ -0,0 +1,11 @@
+public class main {
+    public static void main(String[] args){
+
+        calculadora calc = new calculadora();
+
+        System.out.println(calc.somar(1.0, 1.0));
+        System.out.println(calc.subtrair(1.0,1.0));
+        System.out.println(calc.multiplicar(1.0,1.0));
+        System.out.println(calc.dividir(1.0,1.0));
+    }
+}
